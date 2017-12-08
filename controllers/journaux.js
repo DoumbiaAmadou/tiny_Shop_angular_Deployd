a7c8d03c7aa9459ad051893ab51371c1b89caeaa
= <=>
2017-12-09 00:16:03 +0100

    paginaor

--- controllers/journaux.js
+++ controllers/journaux.js
@@ -1,16 +1,23 @@
-
 window.myApp =angular.module("journaux" , ['customFilters']);
 myApp.controller("journauxCtrl", ['$scope' , function ($scope) {
 	
 	$scope.data = {
 		products: [
-		{ name: "Product #1", description: "A product",
-		category: "Category #1", price: 100 },
-		{ name: "Product #2", description: "A product",
-		category: "Category #1", price: 110 },
-		{ name: "Product #3", description: "A product",
-		category: "Category #2", price: 210 },
-		{ name: "Product #4", description: "A product",
-		category: "Category #3", price: 202 }]
+		{ name: "Product #1", description: "A product",	category: "Category #1", price: 60 },
+		{ name: "Product #2", description: "A product",	category: "Category #1", price: 410 },
+		{ name: "Product #3", description: "A product",category: "Category #2", price: 310 },
+		{ name: "Product #4", description: "A product",category: "Category #3", price: 20 },
+		{ name: "Product #5", description: "A product",category: "Category #3", price: 15 },
+		{ name: "Product #6", description: "A product",category: "Category #3", price: 29 },
+		{ name: "Product #7", description: "A product",category: "Category #2", price: 27 },
+		{ name: "Product #8", description: "A product",category: "Category #2", price: 62 },
+		{ name: "Product #9", description: "A product",category: "Category #3", price: 32 },
+		{ name: "Product #10", description: "A product",category: "Category #3", price: 62 },
+		{ name: "Product #11", description: "A product",category: "Category #1", price: 92 },
+		{ name: "Product #12", description: "A product",category: "Category #1", price: 52 },
+		{ name: "Product #13", description: "A product",category: "Category #3", price: 42 },
+		
+
+		]
 	};
 }]);
--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -1,15 +1,30 @@
-myApp.constant("productListActiveClass", "btn-primary")
-.controller("productListCtrl" , ($scope , $filter,productListActiveClass) =>{
+myApp.constant("productListActiveClass", "active btn-primary")
+.constant("pagniner" , 4)
+.controller("productListCtrl" , ($scope , $filter,productListActiveClass , pagniner) =>{
 	var selectedCategory   = null ; 
+	$scope.currentPage =1 ; 
+	$scope.pageSize = pagniner;
+	
 	$scope.selectCategory = function (newCategory) {
 		selectedCategory = newCategory;
+		$scope.currentPage =1;
 	}
 	
 	$scope.categoryFilterFn = function (product) {
 		return selectedCategory == null ||
 		product.category == selectedCategory;
 	}
+	
+	$scope.selectPage = function(page){
+		console.log("amadou ");
+		$scope.currentPage = page;
+	}
 	$scope.getCategoryClass = function (category) {
 		return selectedCategory == category ? productListActiveClass : "";
 	}
+	$scope.getPageClass = (page)=>{
+		return $scope.currentPage == page ? productListActiveClass:"" ; 
+	}
+
+
 });
