6f353ab170fa9fb0b94842ad4b993c093a66b641
= <=>
2018-07-13 14:44:25 +0200

    last

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/PrivateViews', express.static(__dirname + '/views'));
 app.use('/PrivateJs', express.static(__dirname + '/js'));
-
+process.title = "Skelethon";
 
 app.use('/', index);
 app.use('/users', users);
